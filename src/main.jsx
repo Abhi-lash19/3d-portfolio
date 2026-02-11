c729ef26282946819e03b2b67ccaae8a03538842
Abhilash Kumar Giri <114879092+Abhi-lash19@users.noreply.github.com>
2026-02-11 06:01:54 +0000

    fix: resolved github issue by bringing old code back

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <App />
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
